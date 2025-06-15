cf07838ebbc7848aeea974b05b86cc366a469135
Zen <mikhail@zenembed.com>
2025-06-15 16:39:14 +0200

    Improve connection to Nextcloud.

--- scripts/nextcloud_sinc.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# nextcloudcmd use ~/.netrc to get login and pass
-if $(nextcloudcmd -n -s --path nextcloud ~/nextcloud "https://cloud.zenembed.com")
-then
-    echo "󰅠"
-else
-    echo "󰧠"
-fi
